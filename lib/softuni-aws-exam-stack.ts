60589036da626b01f35c15cc0dfc309f308a3561
Lyubomir Bozhilov <lyubomir555@gmail.com>
2024-12-01 09:34:31 +0100

    WIP threshold alarm

--- lib/softuni-aws-exam-stack.ts
+++ lib/softuni-aws-exam-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from "aws-cdk-lib";
+import { Alarm, ComparisonOperator, Metric } from "aws-cdk-lib/aws-cloudwatch";
+import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
 import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Topic } from "aws-cdk-lib/aws-sns";
 import { EmailSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
@@ -28,6 +30,24 @@ export class SoftuniAwsExamStack extends cdk.Stack {
     thresholdReachedTopic.addSubscription(
       new EmailSubscription("lyubomir555@gmail.com")
     );
+
+    // CloudWatch Metric for the DynamoDB table's item count
+    const itemCountMetric = productTable.metric('Table.ItemCount', {
+      statistic: 'Sum',
+      period: cdk.Duration.minutes(1)
+    });
+
+    // CloudWatch Alarm
+    const alarm = new Alarm(this, 'ItemCountAlarm', {
+      metric: itemCountMetric,
+      // TODO: Change to 10
+      threshold: 2,
+      evaluationPeriods: 1,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+    });
+
+    // Add SNS Topic as Alarm Action
+    alarm.addAlarmAction(new SnsAction(thresholdReachedTopic));
 
     // Outputs
     new cdk.CfnOutput(this, "TableArn", {
