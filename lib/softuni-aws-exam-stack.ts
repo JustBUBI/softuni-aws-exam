3010ccd06f45d615f0b15eb97e0642562707a27a
Lyubomir Bozhilov <lyubomir555@gmail.com>
2024-12-01 08:54:04 +0100

    Create dynamo table with secondary index

--- lib/softuni-aws-exam-stack.ts
+++ lib/softuni-aws-exam-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
 
 export class SoftuniAwsExamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB table
+    const productTable = new Table(this, "ProductTable", {
+      tableName: "ProductTable",
+      partitionKey: { name: "productId", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+    // Add global secondary index to query by tag
+    productTable.addGlobalSecondaryIndex({
+      indexName: "TagIndex",
+      partitionKey: { name: "tag", type: AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SoftuniAwsExamQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Outputs
+    new cdk.CfnOutput(this, "TableArn", {
+      value: productTable.tableArn,
+      description: "ARN of the DynamoDB Product Table",
+    });
   }
 }
